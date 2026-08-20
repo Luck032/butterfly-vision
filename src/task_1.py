59d3f4ac9d7c88af786fa4a6d6e6011c0917d05f
Ilija Tanaskovic <ilijatanaskovic97@hotmail.com>
2026-08-20 12:03:31 +0200

    Changed to show image using matplotlib as in task 2

--- src/task_1.py
+++ src/task_1.py
@@ -1,5 +1,7 @@
 import cv2
 import os
+import matplotlib.pyplot as plt
+
 
 def main():
     work_dir = "./CV_dataset"
@@ -8,12 +10,12 @@ def main():
     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     blurred_img = cv2.GaussianBlur(gray_image, (5, 5), 0)
 
-    cv2.imshow("Original", img)
-    cv2.imshow("Grayscale", gray_image)
-    cv2.imshow("Gaussian blur", blurred_img)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    plt.figure()
+    plt.subplot(2, 1, 1)
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.subplot(2, 1, 2)
+    plt.imshow(cv2.cvtColor(blurred_img, cv2.IMREAD_GRAYSCALE))
+    plt.show()
 
 if __name__ == "__main__":
     main()
